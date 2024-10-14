4d4130c41832febde636dcdf9bc5a769372ecca0
SayRetrast <retrast.keke3h@gmail.com>
2024-10-14 15:48:09 +0700

    add sign in form layout

--- client-app/src/modules/auth/components/SignInForm.vue
+++ client-app/src/modules/auth/components/SignInForm.vue
@@ -1,5 +1,21 @@
+<script setup lang="ts">
+import { Button } from '@/core/components/ui/button';
+import { Input } from '@/core/components/ui/input';
+import { Label } from '@/core/components/ui/label';
+</script>
+
 <template>
   <form>
-    <h1>Sign In</h1>
+    <div>
+      <Label>Username</Label>
+      <Input type="text" placeholder="Your username" />
+    </div>
+
+    <div>
+      <Label>Password</Label>
+      <Input type="password" placeholder="Your password" />
+    </div>
+
+    <Button class="mt-3 w-full" variant="outline">Sign in</Button>
   </form>
 </template>
--- client-app/src/modules/auth/views/SignInView.vue
+++ client-app/src/modules/auth/views/SignInView.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+import SignInForm from '../components/SignInForm.vue';
+</script>
+
 <template>
-  <h1>Sign In Page</h1>
+  <div class="mt-72 flex h-full items-center justify-center">
+    <SignInForm />
+  </div>
 </template>
