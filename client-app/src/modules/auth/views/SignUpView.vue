2cbf8209f32323a72b970e427ed157ade850b3b7
SayRetrast <retrast.keke3h@gmail.com>
2024-10-14 16:01:11 +0700

    create sign up route, form, view

---
+++ client-app/src/modules/auth/views/SignUpView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import SignUpForm from '../components/SignUpForm.vue';
+</script>
+
+<template>
+  <div class="mt-72 flex h-full items-center justify-center">
+    <SignUpForm />
+  </div>
+</template>
