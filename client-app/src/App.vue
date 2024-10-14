3b67948f3c433a5e0152beecf54f9d36904e0cd4
SayRetrast <retrast.keke3h@gmail.com>
2024-10-14 15:10:16 +0700

    delete sign up page

--- client-app/src/App.vue
+++ client-app/src/App.vue
@@ -1,12 +1,5 @@
 <template>
   <main>
-    <div>
-      <RouterLink to="/sign-in">To sign in page</RouterLink>
-      <RouterLink to="/sign-up">To sign up page</RouterLink>
-    </div>
-
-    <p>Current route path is: {{ $route.fullPath }}</p>
-
     <RouterView />
   </main>
 </template>
---
+++ client-app/src/modules/auth/components/SignInForm.vue
@@ -0,0 +1,5 @@
+<template>
+  <form>
+    <h1>Sign In</h1>
+  </form>
+</template>
