7213b286cb3f12e0298c17c3706bf7e665c4763e
SayRetrast <retrast.keke3h@gmail.com>
2024-10-20 23:05:58 +0700

    add user repository

---
+++ server-app/src/core/providers/user.repository.ts
@@ -0,0 +1,29 @@
+import { IUser } from '../interfaces/user.interface';
+import prisma from '../utils/prisma';
+
+export interface IUserRepository {
+  getUser(userId: string): Promise<IUser | null>;
+  createUser(username: string): Promise<IUser>;
+}
+
+export class UserRepository implements IUserRepository {
+  async getUser(userId: string): Promise<IUser | null> {
+    const user = await prisma.users.findUnique({
+      where: {
+        userId: userId,
+      },
+    });
+
+    return user;
+  }
+
+  async createUser(username: string): Promise<IUser> {
+    const createdUser = await prisma.users.create({
+      data: {
+        username: username,
+      },
+    });
+
+    return createdUser;
+  }
+}
