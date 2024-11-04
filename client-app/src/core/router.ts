854374794f7e3f8e09f9e0243c615d5154693414
SayRetrast <retrast.keke3h@gmail.com>
2024-11-04 22:18:04 +0700

    add page not found view

--- client-app/src/core/router.ts
+++ client-app/src/core/router.ts
@@ -1,7 +1,11 @@
 import { createWebHistory, createRouter } from 'vue-router';
 import { authRoutes } from '@/modules/auth';
+import PageNotFoundView from './views/PageNotFoundView.vue';
 
 export const router = createRouter({
   history: createWebHistory(),
-  routes: [...authRoutes],
+  routes: [
+    ...authRoutes,
+    { path: '/:pathMatch(.*)*', component: PageNotFoundView },
+  ],
 });
---
+++ client-app/src/core/views/PageNotFoundView.vue
@@ -0,0 +1,5 @@
+<!-- <script lang="ts"></script> -->
+
+<template>
+  <div class="mt-72 text-center text-2xl">Page not found</div>
+</template>
